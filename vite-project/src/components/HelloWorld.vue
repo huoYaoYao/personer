63bdfb492e86356f7f9d3b64c7062af23b115bd7
huoyaoyao <2242659733@qq.com>
2022-09-23 17:58:32 +0800

    配置项目

--- vite-project/src/components/HelloWorld.vue
+++ vite-project/src/components/HelloWorld.vue
@@ -1,38 +1,25 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+  import { onMounted, computed } from 'vue'
+  import { useStore } from 'vuex'
+  import { login } from '../api/user'
 
-defineProps<{ msg: string }>()
+  const store = useStore()
+  const count = computed(() => {
+    return store.state.count
+  })
+  const onClick = () => {
+    store.commit('increment')
+  }
 
-const count = ref(0)
+  onMounted(() => {
+    login({ account: 'admin', password: '123456' }).then(res => {
+      console.log(res)
+    })
+  })
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <button type="button" @click="onClick">count is: {{ count }}</button>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
+<style scoped></style>
--- vite-project/src/main.ts
+++ vite-project/src/main.ts
@@ -1,5 +1,13 @@
+// main.ts
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import router from './router/index'
+import store from './store/index'
+import ElementPlus from 'element-plus' // 引入element-plus
+import 'element-plus/dist/index.css' // 引入element-plus的样式
+import './mock'
+const app = createApp(App)
+app.use(router)
+app.use(store)
+app.use(ElementPlus) // use element-plus 
+app.mount('#app')
